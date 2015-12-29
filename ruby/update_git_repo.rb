d585fa17501ff3ce163255a8f8d8631129194284
Satyajit Roy <email2sroy@gmail.com>
2015-12-29 14:08:05 -0800

    Adding new ruby script for auto updating the git repos

---
+++ ruby/update_git_repo.rb
@@ -0,0 +1,14 @@
+# Author: Satyajit Roy
+# Date: 12/29/2015
+# Purpose: I usually work with load of repos and time to time I need to make sure that I have the latest and the greatest.
+
+require 'Pathname'
+require 'colorize'
+
+Pathname.new('/src/repo').children.select { |c| c.directory? }.collect { |p| p.to_s }.each do |reponame|
+	Dir.chdir("#{reponame}")
+	puts "Doing git pull on repo:  ".green + "#{reponame}".yellow
+	`git pull --rebase`
+end
+
+#TODO: Email notification for the updated repos
