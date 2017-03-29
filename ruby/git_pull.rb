361aab33f66f9863b72281e713d8fbaa571ec747
Satyajit Roy <kodelint@gmail.com>
2017-03-29 15:53:33 -0700

    Needed to update the repos automatically for both work and personal work

--- ruby/git_pull.rb
+++ ruby/git_pull.rb
@@ -1,10 +1,20 @@
 require 'Pathname'
 require 'colorize'
 
-# I worte this because I wanted to pull all update first things in morning 
+repo = ARGV[0]
+work = 'PATH_2_WORK'
+open = 'PATH_2_PERSONAL'
 
-Pathname.new('REPO_PATH').children.select { |c| c.directory? }.collect { |p| p.to_s }.each do |reponame|
-	Dir.chdir("#{reponame}")
-	puts "Doing git pull on repo:  ".green + reponame.split('/').last.yellow
-	`git pull --rebase`
+if repo == 'work' 
+  Pathname.new("#{work}").children.select { |c| c.directory? }.collect { |p| p.to_s }.each do |reponame|
+  	Dir.chdir("#{reponame}")
+  	puts "Doing git pull on repo:  ".green + reponame.split('/').last.yellow
+  	`git pull --rebase`
+  end
+else
+  Pathname.new("#{open}").children.select { |c| c.directory? }.collect { |p| p.to_s }.each do |reponame|
+  	Dir.chdir("#{reponame}")
+  	puts "Doing git pull on repo:  ".green + reponame.split('/').last.yellow
+  	`git pull --rebase`
+  end
 end
